51481806dcecc3ff1fdb218d95f8a83f15f5f7d5
Shirish Harishchandra Jaiswal <shirish.jaiswal@arrise.com>
2026-05-05 23:28:19 +0530

    logout -m

--- lib/api/auth/logout/request-logout.ts
+++ lib/api/auth/logout/request-logout.ts
@@ -2,9 +2,14 @@ import axios from "axios";
 
 export async function c_logout() {
   try {
-    await axios.post("/portal/api/auth/logout", {});
+    await axios.post(
+      "/portal/api/auth/logout",
+      {},
+      {
+        withCredentials: true,
+      }
+    );
   } catch (error: any) {
-    console.log(error);
     throw new Error(error?.message || "Failed to logout");
   }
 }
